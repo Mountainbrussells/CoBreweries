4f319a7f38eb6fc3250cba198a3015e7801af475
Mountainbrussells <mountainbrussells@gmail.com>
2016-01-06 10:38:30 -0700

    Add puplic properties to BeerDtailVC

--- ColoradoBreweryFinder/CBFBeerRatingViewController.h
+++ ColoradoBreweryFinder/CBFBeerRatingViewController.h
@@ -7,7 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CBFServiceController.h"
+#import "CBFCoreDataController.h"
+#import "CBFUser.h"
+#import "CBFBeer.h"
+#import <CoreData/CoreData.h>
 
 @interface CBFBeerRatingViewController : UIViewController
+
+@property (strong, nonatomic)CBFServiceController *serviceController;
+@property (strong, nonatomic)CBFCoreDataController *coredataController;
+@property (strong, nonatomic)NSManagedObjectID *userManagedObjectId;
+@property (strong, nonatomic)NSManagedObjectID *beerManagedObjectId;
 
 @end
