c9b8cf0714a5b218d347fd60bd67220c1932d7d8
Mountainbrussells <mountainbrussells@gmail.com>
2016-01-06 10:17:41 -0700

    Modified cloud code

--- parseCloudCode/cloud/main.js
+++ parseCloudCode/cloud/main.js
@@ -4,11 +4,25 @@ Parse.Cloud.define("hello", function(request, response) {
 
 Parse.Cloud.beforeSave("BeerRating", function(request, response) {
   console.log("Before save triggered")
-  var user = request.user();
-  console.log("got user");
-  var username = user.getUsername();
-  console.log("Here is the username: " + username);
-  request.object.username = username;
+  var userDict = request.object.get("user");
+  console.log(userDict);
+  var userId = userDict["objectId"];
+  console.log(userId);
+  var query = new Parse.Query(Parse.User);
+  query.equalTo("userId", userId);
+  query.find({
+    success: function(results) {
+      console.log(results);
+      var user = results[0];
+      var username = user.getUsername();
+      console.log("Here is the username: " + username);
+      request.object.username = username;
+    },
+    error: function(){
+      console.error();
+    }
+  });
+
   response.success()
 })
 
