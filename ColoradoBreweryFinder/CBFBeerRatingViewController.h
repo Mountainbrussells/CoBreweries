fa5fb6e9b9d35e5fa4aa77802f06df9ded52dc63
Mountainbrussells <mountainbrussells@gmail.com>
2016-01-06 10:25:32 -0700

    Add BeerRatingVC.h and .m

---
+++ ColoradoBreweryFinder/CBFBeerRatingViewController.h
@@ -0,0 +1,13 @@
+//
+//  CBFBeerRatingViewController.h
+//  ColoradoBreweryFinder
+//
+//  Created by Ben Russell on 1/6/16.
+//  Copyright © 2016 Ben Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBFBeerRatingViewController : UIViewController
+
+@end
